7ed8099faf253a1e3abe7088e1ecc6de83a5c2e5
Emman-pip <parrenoemmanuel756@gmail.com>
2024-11-20 15:04:55 +0800

    Add encryption utility.

---
+++ src/main/java/com/main/invento/utilityClasses/Encryptor.java
@@ -0,0 +1,15 @@
+package com.main.invento.utilityClasses;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encryptor {
+    public static String SHA256(String input) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(input.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+        byte[] hashed = digest.digest();
+        return String.format("%64x", new BigInteger(1, hashed));
+    }
+}
